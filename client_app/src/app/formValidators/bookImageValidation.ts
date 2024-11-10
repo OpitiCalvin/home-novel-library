afb6de9cd43f229d9608ad07be26ccab707a1bc3
Opiti, Calvin <opiticalvin@gmail.com>
2024-11-10 10:54:25 +0000

    added functionality for server side validation of uploaded book images

--- client_app/src/app/formValidators/bookImageValidation.ts
+++ client_app/src/app/formValidators/bookImageValidation.ts
@@ -1,22 +1,12 @@
 import { zfd } from "zod-form-data";
 import {z} from "zod";
 
-const MAX_FILE_SIZE = 5000000;
-const ACCEPTED_IMAGE_TYPES = ['image/jpeg','image/jpg', 'image/png', 'image/webp']
+const MAX_FILE_SIZE = 50000000;
 
 export const bookImageFormSchema = zfd.formData({
-  title: zfd.text(z.string().min(2, "Too short").max(50, "Too long")),
-  // authorId: zfd.numeric(z.number()),
-  // genreId: zfd.numeric(z.number()),
-  // publishedYear: zfd.numeric(z.number()),
-  // isbn: zfd.text(z.string().min(2, "Too short").max(20, "Too long")),
-  // description: zfd.text(z.string().min(10, "Too short").max(250, "Too long")),
-  // availabilityStatus: zfd.text(z.string().min(2, "Too short").max(20, "Too long")),
-  // readStatus: zfd.text(z.string().min(2, "Too short").max(20, "Too long")),
-  coverImage: typeof window === 'undefined' ?z.any() : z.instanceof(FileList)
-    .refine(files => files?.length == 1, "File is required")
-    .refine((files) => files?.[0]?.size <= MAX_FILE_SIZE, 'Max image size is 5MB.')
-    .refine(files => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-      "Only .jpg, .jpeg, .png and .webp formats are supported."
-    )
+  bookId: zfd.numeric(z.number().int().min(1, {message:"A book is required"}).positive({message: "Bood ID must be a positive number"})),
+  coverImages: z.any()
+    .refine((files:FileList) => files && Array.from(files).length >= 1, "At least 1 File is required")
+    .refine((files:FileList) => Array.from(files).every((file: File) => file.size <= MAX_FILE_SIZE), 'Max image size is 5MB.')
+    .refine((files:FileList) => Array.from(files).every((file: File) => file.type.startsWith('image/')), "Only image files are allowed"),
 });

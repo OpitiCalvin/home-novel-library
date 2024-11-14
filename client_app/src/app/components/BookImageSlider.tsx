78938b92b00b4667672bc22d32a1df61af727745
Opiti, Calvin <opiticalvin@gmail.com>
2024-11-14 22:53:36 +0000

    implemented a slider for individual book cover images

---
+++ client_app/src/app/components/BookImageSlider.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from 'react'
+import useSWR from 'swr';
+import { IBookImageResponse } from '../utils/schemas';
+import ImageSlider from './ImageSlider';
+
+type Props = {
+    bookId: number;
+}
+const BookImageSlider: FunctionComponent<Props> = ({bookId}) => {
+
+    const {data, error, isLoading } = useSWR(`books/${bookId}/images`);
+
+    if (error) return <div>Failed to Load.</div>;
+    if (isLoading) return <div>Loading book cover images...</div>;
+    if (data) {
+        const bookImages: IBookImageResponse[] = data["images"];
+
+        return (
+        <div>
+          <h3>Cover Images</h3>
+          <hr />
+          <ImageSlider images={bookImages} />
+        </div>
+        )
+
+    }
+}
+
+export default BookImageSlider

d768934b97abff6bdaee15b527fa43b13750269a
Opiti, Calvin <opiticalvin@gmail.com>
2024-11-14 22:58:20 +0000

    swapped table for updated bookcard component

--- client_app/src/app/authors/[slug]/page.tsx
+++ client_app/src/app/authors/[slug]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { fetcher } from "@/api/apiFetcher";
+import BookCard from "@/app/components/BookCard";
 import { IAuthorAndBooksResponse } from "@/app/utils/schemas";
 import { useParams } from "next/navigation";
 import React from "react";
@@ -31,32 +32,10 @@ const SingleAuthor: React.FC = () => {
             <h1 className="text-center text-4xl font-bold text-gray-800 mb-8">
               Pubished Books
             </h1>
-            <div className="flex items-center sm:justify-center ml-4 sm:ml-0">
-              <table className="md:table-fixed">
-                <thead className="sr-only">
-                  <tr className="tr-class">
-                    <th>Title</th>
-                    <th>Year Published</th>
-                    <th>ISBN</th>
-                    <th>Available?</th>
-                    <th>Read Status</th>
-                    {/* <th>Description</th> */}
-                  </tr>
-                </thead>
-                <tbody>
-                  {author.books.map((book, index) => (
-                    <tr key={index} className="tr-class">
-                      <td className="td-class">{book.title}</td>
-                      <td className="td-class">{book.publishedYear}</td>
-                      <td className="td-class">{book.isbn}</td>
-                      <td className="td-class">{book.availabilityStatus}</td>
-                      <td className="td-class">{book.readStatus}</td>
-                      {/* <td className="td-class" style={{maxWidth: "200px"}}>{book.description}</td> */}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+            {/* Author's Published Books */}
+            {author.books.map((book, index) => (
+            <BookCard key={index} book={book} />
+            ))}
           </section>
         )}
       </>

142a8fafdbb1fba9b1d96fc9c92dc9fb0033712a
Opiti, Calvin <opiticalvin@gmail.com>
2024-11-16 08:01:11 +0000

    updated auth middleware and added admin and basic user middlewares

--- nodejs_server/middleware/auth.js
+++ nodejs_server/middleware/auth.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
 
-const auth = (req, res, next) => {
+exports.auth = (req, res, next) => {
   const token = req.header("Authorization");
   if (!token) return res.status(401).send("Access denied.");
 
   try {
-    const verified = jwt.verify(token, "secretKey");
+    const verified = jwt.verify(token, jwtSecret);
     req.user = verified;
     next();
   } catch (err) {
@@ -13,4 +14,42 @@ const auth = (req, res, next) => {
   }
 };
 
-module.exports = auth;
+exports.adminAuth = (req, res, next) => {
+  const token = req.cookies.jwt
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({message: "Not Authorized"})
+      } else {
+        if (decodedToken.role !== "Admin") {
+          return res.status(401).json({message: "Not Authorized"})
+        } else {
+          next()
+        }
+      }
+    })
+  } else {
+    return res.status(401).json({ message: "Not Authorized, token not available." });
+  }
+}
+
+exports.userAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not Authorized" });
+      } else {
+        if (decodedToken.role !== "Member") {
+          return res.status(401).json({ message: "Not Authorized" });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Not Authorized, token not available." });
+  }
+};

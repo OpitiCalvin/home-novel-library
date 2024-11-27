675957b3f52135c592df3e97d450319fade21ded
Opiti, Calvin <opiticalvin@gmail.com>
2024-11-27 15:38:55 +0000

    added containerisation and build params

--- homelib/next.config.ts
+++ homelib/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.opititechgeolabs.com",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.0.69",
+      },
+    ],
+  },
+  output: "standalone",
 };
 
 export default nextConfig;

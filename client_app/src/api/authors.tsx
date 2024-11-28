d82a511746f8e34ee6a6af11fc85914b0adc82bd
Opiti, Calvin <opiticalvin@gmail.com>
2024-11-28 18:06:00 +0000

    deleted. nolonger needed

--- client_app/src/api/authors.tsx
+++
@@ -1,7 +0,0 @@
-export async function getAllAuthors() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/authors`, {
-    next: { revalidate: 180 },
-  });
-  const apiAuthors = await res.json();
-  return apiAuthors["authors"];
-}
--- client_app/src/api/genres.ts
+++
@@ -1,7 +0,0 @@
-export async function getAllGenres(){
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/genres`, {
-    next: { revalidate: 180 },
-  });
-  const apiGenres = await res.json();
-  return apiGenres["genres"];
-}

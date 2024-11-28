4a8b4261e3bb2418e2917528817cc29be2632c9b
Opiti, Calvin <opiticalvin@gmail.com>
2024-11-28 10:44:09 +0000

    added genre category to card

--- homelib/src/components/GenreCard.tsx
+++ homelib/src/components/GenreCard.tsx
@@ -10,11 +10,14 @@ type Props = {
 const GenreCard: FunctionComponent<Props> = ({ genre }) => {
   return (
     <div className="bg-white shadow-md rounded-md p-4 text-center">
-      <h2 className="text-2xl font-semibold text-gray-800">{genre.name}</h2>
+      <h5 className="text-2xl font-semibold text-blue-800">{genre.name}</h5>
       <p className="text-sm text-gray-500">{genre.description}</p>
+      <p className="text-lg font-semibold text-gray-600">{genre.category}</p>
       <hr className="my-2" />
       <div>
-        <Link href={`/genres/${genre.id}`}>
+        <Link href={`/genres/${genre.id}`}
+        // className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
           View Info & Books <FontAwesomeIcon icon={faArrowRight} />
         </Link>
       </div>

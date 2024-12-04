c4dfa518906c2fffe57d6e737a25f216c30196bb
Opiti, Calvin <opiticalvin@gmail.com>
2024-12-04 13:26:01 +0000

    implemented route protection to check if user is logged in before loading the page

--- homelib/src/app/add-book/page.tsx
+++ homelib/src/app/add-book/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { BookForm } from "../../components/bookForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const AddBook: React.FC = () => {
+const AddBook: React.FC = async () => {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/auth/login");
+  }
   return <BookForm />;
 };
 

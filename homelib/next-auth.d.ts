edb6ed0c16ae619a15841e556bc91fbcb1f66d63
Opiti, Calvin <opiticalvin@gmail.com>
2024-12-05 17:19:50 +0000

    still figuring this out

--- homelib/next-auth.d.ts
+++ homelib/next-auth.d.ts
@@ -1,17 +1,18 @@
-import { User } from "@/database/models";
-// import "next-auth"
-// import { string } from "zod";
-
 declare module "next-auth/providers/credentials" {
-  interface User {
-    email: string;
-    name: string;
-    access_token: string;
-    refresh_token: string;
-    expires_on: number;
-    exp: number;
-    iat: number;
-    jti: string;
+  // interface User {
+  //   email: string;
+  //   name: string;
+  //   access_token: string;
+  //   refresh_token: string;
+  //   expires_on: number;
+  //   exp: number;
+  //   iat: number;
+  //   jti: string;
+  // }
+
+  interface extUser extends User {
+    role?: string;
+
   }
 
   interface Session extends DefaultSession {
@@ -30,16 +31,3 @@ declare module "next-auth/providers/credentials" {
     authorize?: (credentials: Credentials) => Promise<any | null>;
   }): any;
 }
-
-// declare module "next-auth/providers/credentials" {
-//   interface Credentials {
-//     email: string;
-//     password: string;
-//   }
-
-//   export default function CredentialsProvider(options: {
-//     name: string;
-//     credentials: Record<string, any>;
-//     authorize?: (credentials: Credentials) => Promise<any | null>;
-//   }): any;
-// }

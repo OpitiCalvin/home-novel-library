a301921ec576e1b48d2f7ff9983ea93d61eda00e
Opiti, Calvin <opiticalvin@gmail.com>
2024-12-05 17:19:19 +0000

    reworked to include a login function

--- homelib/src/dal/users.ts
+++ homelib/src/dal/users.ts
@@ -1,25 +1,20 @@
 import { User } from "@/database/models";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import bcrypt from "bcrypt";
 
-export async function getUsers(limited: boolean = true) {
+export async function getUsers() {
   const session = await getServerSession();
   if (!session) {
     redirect("/auth/login");
   }
 
   try {
-    const users = limited
-      ? await User.findAll({
-          attributes: {
-            exclude: ["password", "createdAt", "updatedAt"],
-          },
-        })
-      : await User.findAll({
-          attributes: {
-            exclude: ["createdAt", "updatedAt"],
-          },
-        });
+    const users = await User.findAll({
+      attributes: {
+        exclude: ["password", "createdAt", "updatedAt"],
+      },
+    });
     return users;
   } catch (error) {
     console.error("An error occurred attempting to retrieve users data", error);
@@ -27,7 +22,7 @@ export async function getUsers(limited: boolean = true) {
   }
 }
 
-export async function getUser(userEmail: string, limited: boolean = true) {
+export async function getUserWithEmail(userEmail: string) {
   const session = await getServerSession();
   if (!session) {
     redirect("/auth/login");
@@ -37,18 +32,38 @@ export async function getUser(userEmail: string, limited: boolean = true) {
     if (!userEmail) {
       return null;
     }
-    const user = limited
-      ? User.findOne({
-          attributes: { exclude: ["password", "createdAt", "updatedAt"] },
-          where: { email: userEmail },
-        })
-      : User.findOne({
-          attributes: { exclude: ["createdAt", "updatedAt"] },
-          where: { email: userEmail },
-        });
+    const user = User.findOne({
+      attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+      where: { email: userEmail },
+    });
     return user;
   } catch (error) {
     console.error("Error retrieving user", error);
+    return null;
+  }
+}
+
+export async function loginUser(email: string, password: string) {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: email.toLowerCase(),
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+    if (!user) {
+      console.error("User not found!");
+      return null;
+    }
+    const isPasswordMatch = await bcrypt.compare(password || "", user.password);
+    if (!isPasswordMatch) {
+      return null;
+    }
+    return { id: user.id, email: user.email, role: user.role };
+  } catch (error) {
+    console.error("Error signing in the user", error);
     return null;
   }
 }

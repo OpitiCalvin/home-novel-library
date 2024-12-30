34761b10f6547b123bf1ed4d0d6f279b6ccc48ef
Opiti, Calvin <opiticalvin@gmail.com>
2024-12-30 15:25:12 +0000

    check for user admin role in session prior to loading the form

--- homelib/src/app/add-book/page.tsx
+++ homelib/src/app/add-book/page.tsx
@@ -4,29 +4,20 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/options";
 
-// interface ExtUser extends User {
-//   role?: string
-// }
-
 const AddBook: React.FC = async () => {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/auth/login");
   }
-  // if (session.user){
-  //   const sessUser: ExtUser = session?.user;
-  //   console.log(sessUser)
-
-  // }
-  // if (sessUser?.role !== "admin") {
-  //   return (
-  //     <div className="bg-white shadow-md rounded-md p-4 text-center">
-  //       <p className="text-lg font-semibold text-gray-600">
-  //         ONLY admins allowed to add book entries.
-  //       </p>
-  //     </div>
-  //   );
-  // }
+  if (session.user?.role !== "Admin") {
+    return (
+      <div className="bg-white shadow-md rounded-md p-4 text-center">
+        <p className="text-lg font-semibold text-gray-600">
+          ONLY admins allowed to add book entries.
+        </p>
+      </div>
+    );
+  }
   return <BookForm />;
 };
 

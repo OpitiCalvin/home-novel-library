ec1f1a0e1656c6c1df7e1a48f011f534f9010593
Opiti, Calvin <opiticalvin@gmail.com>
2024-12-30 21:44:45 +0000

    protected genre post api endpoint

--- homelib/src/app/api/genres/route.ts
+++ homelib/src/app/api/genres/route.ts
@@ -2,6 +2,7 @@ import { Genre } from "@/database/models";
 import sequelize from "@/database/models/connection";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/options";
 
 export const GET = async () => {
   try {
@@ -22,7 +23,7 @@ export const GET = async () => {
 };
 
 export const POST = async (req: Request) => {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.json({ message: "Not Authenticated" }, { status: 401 });
   }

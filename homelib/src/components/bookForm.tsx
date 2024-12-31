ac4afa8a8d8367fb53b4cd1aba65cc3e12d7949f
Opiti, Calvin <opiticalvin@gmail.com>
2024-12-31 01:13:00 +0000

    updated form to not use server actions

--- homelib/src/components/bookForm.tsx
+++ homelib/src/components/bookForm.tsx
@@ -1,169 +1,48 @@
 "use client";
 
-import React, { useEffect, useActionState } from "react";
-import {
-  useForm,
-  UseFormRegister,
-  FieldErrors,
-  FieldPath,
-} from "react-hook-form";
-import { State } from "../formValidators/formStates";
-import { processAddBook } from "../actions/addBook";
+import React from "react";
+import { useForm } from "react-hook-form";
 import { bookFormSchema } from "../formValidators/addBookValidation";
-import { useFormStatus } from "react-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ErrorMessage } from "@hookform/error-message";
 import { AuthorSelect } from "./AuthorSelect";
 import { IBook } from "@/lib/schemas";
 import { GenreSelect } from "./genreSelect";
+import { toast } from "sonner";
 
 const inputClasses =
   "px-4 py-2 block border-0 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 border-gray-400 w-full";
 const buttonClasses =
   "w-full rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 disabled:bg-gray-400 disabled:text-gray-300 disabled:cursor-not-allowed";
 
-export function FormContent({
-  register,
-  isValid,
-  errors,
-}: {
-  register: UseFormRegister<IBook>;
-  isValid: boolean;
-  errors: FieldErrors<IBook>;
-}) {
-  const { pending } = useFormStatus();
-
-  return (
-    <>
-      <div className="mb-4">
-        <label className="block text-gray-700">Book Title</label>
-        <input
-          {...register("title")}
-          placeholder="Book Title"
-          type="text"
-          name="title"
-          className={inputClasses}
-        />
-        <span className="text-red-500 font-semibold text-sm">
-          <ErrorMessage name="title" errors={errors} />
-        </span>
-      </div>
-      <AuthorSelect register={register} errors={errors} />
-      <GenreSelect register={register} errors={errors} />
-      <div className="mb-4">
-        <label className="block text-gray-700">Year</label>
-        <input
-          {...register("publishedYear")}
-          type="number"
-          name="publishedYear"
-          placeholder="Publication Year"
-          className={inputClasses}
-        />
-        <span className="text-red-500 font-semibold text-sm">
-          <ErrorMessage name="publishedYear" errors={errors} />
-        </span>
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">ISBN</label>
-        <input
-          {...register("isbn")}
-          type="text"
-          name="isbn"
-          placeholder="ISBN"
-          className={inputClasses}
-        />
-        <span className="text-red-500 font-semibold text-sm">
-          <ErrorMessage name="isbn" errors={errors} />
-        </span>
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Availability Status</label>
-        <select
-          {...register("availabilityStatus")}
-          name="availabilityStatus"
-          className={inputClasses}
-        >
-          <option value="Available">Available</option>
-          <option value="Loaned Out">Loaned Out</option>
-        </select>
-        <span className="text-red-500 font-semibold text-sm">
-          <ErrorMessage name="availabilityStatus" errors={errors} />
-        </span>
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Read Status</label>
-        <select
-          {...register("readStatus")}
-          name="readStatus"
-          className={inputClasses}
-        >
-          <option value="Not Started">Not Started</option>
-          <option value="In Progress">In Progress</option>
-          <option value="Finished">Finished</option>
-          <option value="Stopped">Stopped</option>
-        </select>
-        <span className="text-red-500 font-semibold text-sm">
-          <ErrorMessage name="readStatus" errors={errors} />
-        </span>
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Description</label>
-        <textarea
-          {...register("description")}
-          name="description"
-          rows={10}
-          placeholder="Description"
-          className={inputClasses}
-        />
-        <span className="text-red-500 font-semibold text-sm">
-          <ErrorMessage name="description" errors={errors} />
-        </span>
-      </div>
-      <button
-        type="submit"
-        className={buttonClasses}
-        disabled={pending || !isValid}
-      >
-        Submit
-      </button>
-      {pending && <span>Loading...</span>}
-    </>
-  );
-}
-
 export function BookForm() {
   const {
     register,
-    formState: { isValid, errors },
-    setError,
+    handleSubmit,
+    formState: { errors },
     reset,
   } = useForm<IBook>({
     mode: "all",
     resolver: zodResolver(bookFormSchema),
   });
-  const [state, formAction] = useActionState<State, FormData>(
-    processAddBook,
-    null
-  );
 
-  useEffect(() => {
-    if (!state) {
-      return;
-    }
-    // in case our form action returns `error` we can now `setError`s
-    if (state.status === "error") {
-      state.errors?.forEach((error) => {
-        setError(error.path as FieldPath<IBook>, {
-          message: error.message,
-        });
+  const onSubmit = async (data: IBook) => {
+    try {
+      const res = await fetch("/api/books", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
       });
-    }
-    if (state.status === "success") {
-      alert(state.message);
+      if (!res.ok) {
+        throw new Error("An error occurred while adding a book record");
+      }
+      const { message } = await res.json();
+      toast.success(message);
       reset();
+    } catch (error) {
+      console.error("An Error occurred", error);
+      toast.error("An error occurred while adding a book record");
     }
-  }, [state, setError, reset]);
-
+  };
   return (
     <section className="grid place-items-center py-8 px-4">
       <h1 className="text-center text-4xl font-bold text-gray-800 mb-8">
@@ -171,9 +50,108 @@ export function BookForm() {
       </h1>
       <form
         className="bg-white p-6 rounded-md shadow-md w-full max-w-md"
-        action={formAction}
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <FormContent register={register} isValid={isValid} errors={errors} />
+        <div className="mb-4">
+          <label className="block text-gray-700">Book Title</label>
+          <input
+            {...register("title")}
+            placeholder="Book Title"
+            type="text"
+            name="title"
+            className={inputClasses}
+          />
+          {errors.title && (
+            <span className="text-red-500 font-semibold text-sm">
+              {errors.title.message}
+            </span>
+          )}
+        </div>
+        <AuthorSelect register={register} errors={errors} />
+        <GenreSelect register={register} errors={errors} />
+        <div className="mb-4">
+          <label className="block text-gray-700">Year</label>
+          <input
+            {...register("publishedYear")}
+            type="number"
+            name="publishedYear"
+            placeholder="Publication Year"
+            className={inputClasses}
+          />
+          {errors.publishedYear && (
+            <span className="text-red-500 font-semibold text-sm">
+              {errors.publishedYear.message}
+            </span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">ISBN</label>
+          <input
+            {...register("isbn")}
+            type="text"
+            name="isbn"
+            placeholder="ISBN"
+            className={inputClasses}
+          />
+          {errors.isbn && (
+            <span className="text-red-500 font-semibold text-sm">
+              {errors.isbn.message}
+            </span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Availability Status</label>
+          <select
+            {...register("availabilityStatus")}
+            name="availabilityStatus"
+            className={inputClasses}
+          >
+            <option value="Available">Available</option>
+            <option value="Loaned Out">Loaned Out</option>
+            <option value="In Wish List">In Wish List</option>
+          </select>
+          {errors.availabilityStatus && (
+            <span className="text-red-500 font-semibold text-sm">
+              {errors.availabilityStatus.message}
+            </span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Read Status</label>
+          <select
+            {...register("readStatus")}
+            name="readStatus"
+            className={inputClasses}
+          >
+            <option value="Not Started">Not Started</option>
+            <option value="In Progress">In Progress</option>
+            <option value="Finished">Finished</option>
+            <option value="Stopped">Stopped</option>
+          </select>
+          {errors.readStatus && (
+            <span className="text-red-500 font-semibold text-sm">
+              {errors.readStatus.message}
+            </span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Description</label>
+          <textarea
+            {...register("description")}
+            name="description"
+            rows={10}
+            placeholder="Description"
+            className={inputClasses}
+          />
+          {errors.description && (
+            <span className="text-red-500 font-semibold text-sm">
+              {errors.description.message}
+            </span>
+          )}
+        </div>
+        <button type="submit" className={buttonClasses}>
+          Submit
+        </button>
       </form>
     </section>
   );
